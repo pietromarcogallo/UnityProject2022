35558b19201480ea453c143625a36f4cb9609a08
elenazattoni <elena1.zattoni@mail.polimi.it>
2022-12-13 14:03:21 +0100

    Connection Scenes

---
+++ E-health-Unity-project/Assets/Game1_2/Scripts/NextGame.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextGame : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Start()
+    {
+        NextScene();
+    }
+}
